440a30664d1fee0a8c11e186df4fc4d1c8b70b3c
Oskar Tyniec <39619380+Rakso12@users.noreply.github.com>
2019-11-05 17:54:06 +0100

    New program - "for" learning

---
+++ learn_1.py
@@ -0,0 +1,18 @@
+#for z krotką
+'''
+names = ("Robert","Andrzej","Kondrad","Kamil","Krzysiek")
+for name in names:
+    print(name);
+'''
+
+#for z przedziałem liczbowym
+'''
+value = 2
+for count in range(1,11): #zakres o 1 do 10
+    result = count * value
+    print(count,"*",value,"=",result)
+'''
+
+for count in range(1,13):
+    count = 13
+    print(count)
